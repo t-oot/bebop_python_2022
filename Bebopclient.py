5f5e941891a5a4ff866057e0d49ab32ae4e64140
t-oot <10348716+t-oot@users.noreply.github.com>
2022-05-31 17:38:22 +0900

    SOCKET FIX

--- Bebopclient.py
+++ Bebopclient.py
@@ -1,7 +1,7 @@
 import socket
 import sys
-#import termios
-#import tty
+# import termios
+# import tty
 import msvcrt
 
 
@@ -23,53 +23,51 @@ class Color:
 
 def send_to(t):
     if t == 0:
-        print("Send to all drones. Send q to quit.")
-        while True:
+        print("Send to all drones. Send \"q\" to quit. Send \"c\" to back.")
+    else:
+        print("Send to drone %s  Send \"q\" to quit. Send \"c\" to back." % target)
+    while True:
+        if t == 0:
             print("All drones > ", end="", flush=True)
-            command = msvcrt.getch()
-            try:
-                command_ = command.decode('UTF-8')
-            except:
-                command_ = "0x"+command.hex()
-                continue
-            #command = sys.stdin.read(1)
-            if command_ == "c":
-                break
+        else:
+            print("drone %s > " % target, end="", flush=True)
+        command = msvcrt.getch()
+        if command == b'\x03':
+            break
+        try:
+            command_ = command.decode('UTF-8')
+        except:
+            print(Color.RED+"invalid literal!", Color.END)
+            continue
+        if command_ == " " and t == 0:
+            print("Programming mode.")
+            print("All drones > ", end="", flush=True)
+            command_ = sys.stdin.readline()
+        if command_ == "c":
+            break
+        if t == 0:
             i = 1
+            print(command_ + " > ", end="", flush=True)
             for s in sockets:
                 s.send(command)
-                print(command_ + " > " + Color.GREEN +
-                      "[drone "+str(i)+"]" + "sent.", Color.END, flush=True)
+                print(Color.GREEN +
+                      "[drone "+str(i)+"]" + "sent. ", Color.END, flush=True, end="")
                 i += 1
-            if command_ == "q":
-                break
-    elif t <= 3:
-        print("Send to drone %s  Send q to quit." % target)
-        while True:
-            print("drone", target, "> ", end="", flush=True)
-            command = msvcrt.getch()
-            try:
-                command_ = command.decode('UTF-8')
-            except:
-                command_ = "0x"+command.hex()
-                continue
-            if command_ == "c":
-                break
+            print("\n", end="", flush=True)
+        else:
             sockets[t-1].send(command)
             print(command_ + " > " + Color.GREEN +
                   "sent.", Color.END, flush=True)
-            if command_ == "q":
-                break
-    else:
-        return
+        if command_ == "q":
+            break
 
 
-#fd = sys.stdin.fileno()
-#old = termios.tcgetattr(fd)
+# fd = sys.stdin.fileno()
+# old = termios.tcgetattr(fd)
 sockets = []
 
-hosts = ["192.168.1.1", "192.168.1.2"]
-#hosts = ["127.0.0.1"]
+# hosts = ["192.168.1.1", "192.168.1.2"]
+hosts = ["127.0.0.1", "127.0.0.1"]
 port = 8080
 nodelay = 1
 
@@ -78,18 +76,19 @@ for h in hosts:
     print(Color.YELLOW+"[drone "+str(i)+"]connecting host:",
           h, " port:", port, Color.END)
     sock = socket.socket()
+    sock.settimeout(2.0)
     sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, nodelay)
     sock.connect((h, port))
     sockets.append(sock)
     print(Color.GREEN + "[drone "+str(i)+"]connected host:",
-          h, " port:", port, Color.END)
+          h, " dport:", port, "source:", sock.getsockname(), Color.END)
     i += 1
 
 
 try:
     # tty.setcbreak(sys.stdin.fileno())
     while True:
-        print("select target(1~3). 0:all 9:end\n>", end="")
+        print("select target(1~8). 0:all 9:end\n>", end="")
         target = sys.stdin.readline()  # 1文字読み込み
         target = target.strip()
         if target.isdecimal():  # 入力が数字かどうか判定
@@ -102,15 +101,24 @@ try:
             else:
                 send_to(int(target))
             print("end")
-            command = "q"
-            for s in sockets:
-                s.send(command.encode('utf-8'))
-            break
+            #command = "q"
+            # for s in sockets:
+            #    s.send(command.encode('utf-8'))
+            # break
         else:
             print(Color.RED+"invalid literal!", Color.END)
 
 finally:
-    pass
-    #termios.tcsetattr(fd, termios.TCSANOW, old)
+    for s in sockets:
+        print(Color.PURPLE+"closing:", s.getsockname()[0]+Color.END)
+        try:
+            s.send("q".encode('utf-8'))
+            print("(sent q command)")
+        except:
+            pass
+        finally:
+            pass
+        s.close()
+    # termios.tcsetattr(fd, termios.TCSANOW, old)
 
 print("end client")
--- Bebopcontroller.py
+++ Bebopcontroller.py
@@ -1,84 +1,116 @@
 import socket
-from pyparrot.Bebop import Bebop
+from threading import Thread
+from sys import argv
+import signal
+import time
 
-s = socket.socket()
+looping = False
 
-port = 8080
-s.bind(('', port)) # socketに名前をつける
 
-print("listening")
-s.listen(5) # 接続待ち
-c, addr = s.accept() # 接続要求の取り出し
+def loop(c):
+    global looping
+    looping = True
+    print("connecting to Bebop2")
+    ##success = bebop.connect(10)
+    success = True
+    print("result:", success)
 
-bebop = Bebop(drone_type="Bebop2")
+    if (success):
+        print("connected to Bebop2!")
+        # print(bebop.sensors.battery)
+        print("sleeping")
+        # bebop.smart_sleep(2)
 
-print("connecting to Bebop2")
-success = bebop.connect(10)
-print(success)
+        # bebop.ask_for_state_update()
 
-if (success):
-    print("sleeping")
-    bebop.smart_sleep(2)
+        # set safe indoor parameters
+        # bebop.set_max_tilt(5)
+        # bebop.set_max_vertical_speed(1)
 
-    bebop.ask_for_state_update()
+        # trying out the new hull protector parameters - set to 1 for a hull protection and 0 without protection
+        # bebop.set_hull_protection(1)
+        try:
+            while True:
+                print("receiving")
+                ch = c.recv(4096).decode()
+                print("received:", ch)
+                if ch == "t":
+                    print("take off")
+                    bebop.safe_takeoff(10)
 
-    # set safe indoor parameters
-    bebop.set_max_tilt(5)
-    bebop.set_max_vertical_speed(1)
+                elif ch == "w":
+                    print("move front")
+                    bebop.fly_direct(roll=0, pitch=50, yaw=0,
+                                     vertical_movement=0, duration=1)
+                elif ch == "s":
+                    print("move back")
+                    bebop.fly_direct(roll=0, pitch=-50, yaw=0,
+                                     vertical_movement=0, duration=1)
+                elif ch == "a":
+                    print("move left")
+                    bebop.fly_direct(roll=-50, pitch=0, yaw=0,
+                                     vertical_movement=0, duration=1)
+                elif ch == "d":
+                    print("move right")
+                    bebop.fly_direct(roll=50, pitch=0, yaw=0,
+                                     vertical_movement=0, duration=1)
 
-    # trying out the new hull protector parameters - set to 1 for a hull protection and 0 without protection
-    bebop.set_hull_protection(1)
+                elif ch == "W":
+                    print("move up")
+                    bebop.fly_direct(roll=0, pitch=0, yaw=0,
+                                     vertical_movement=20, duration=0.5)
+                elif ch == "S":
+                    print("move down")
+                    bebop.fly_direct(roll=0, pitch=0, yaw=0,
+                                     vertical_movement=-20, duration=0.5)
+                elif ch == "A":
+                    print("move clockwise")
+                    bebop.fly_direct(roll=0, pitch=0, yaw=25,
+                                     vertical_movement=0, duration=1)
+                elif ch == "D":
+                    print("move conclockwise")
+                    bebop.fly_direct(roll=0, pitch=0, yaw=-25,
+                                     vertical_movement=0, duration=1)
 
-    while True:
-        print("receiving")
-        ch = c.recv(4096).decode()
-    
-        if ch == "t":
-            print("take off")
-            bebop.safe_takeoff(10)
+                elif ch == "f":
+                    print("flip")
+                    bebop.flip(direction="front")
 
-        elif ch == "w":
-            print("move front")
-            bebop.fly_direct(roll=0, pitch=50, yaw=0, vertical_movement=0, duration=1)
-        elif ch == "s":
-            print("move back")
-            bebop.fly_direct(roll=0, pitch=-50, yaw=0, vertical_movement=0, duration=1)
-        elif ch == "a":
-            print("move left")
-            bebop.fly_direct(roll=-50, pitch=0, yaw=0, vertical_movement=0, duration=1)
-        elif ch == "d":
-            print("move right")
-            bebop.fly_direct(roll=50, pitch=0, yaw=0, vertical_movement=0, duration=1)
+                elif ch == "l":
+                    print("land")
+                    bebop.safe_land(10)
 
-        elif ch == "W":
-            print("move up")
-            bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=20, duration=0.5)
-        elif ch == "S":
-            print("move down")
-            bebop.fly_direct(roll=0, pitch=0, yaw=0, vertical_movement=-20, duration=0.5)
-        elif ch == "A":
-            print("move clockwise")
-            bebop.fly_direct(roll=0, pitch=0, yaw=25, vertical_movement=0, duration=1)
-        elif ch == "D":
-            print("move conclockwise")
-            bebop.fly_direct(roll=0, pitch=0, yaw=-25, vertical_movement=0, duration=1)
-
-        elif ch == "f":
-            print("flip")
-            bebop.flip(direction="front")
-
-        elif ch == "l":
-            print("land")
+                elif ch == "q" or len(ch) == 0:
+                    print("end")
+                    bebop.safe_land(10)
+                    break
+        except socket.error:
+            print("socket error")
             bebop.safe_land(10)
-
-        elif ch == "q" or len(ch) == 0:
-            print("end")
-            bebop.safe_land(10)
+        finally:
+            looping = False
             c.close()
-            break
-        
 
-    print("DONE - disconnecting")
-    bebop.smart_sleep(5)
-    print(bebop.sensors.battery)
-    bebop.disconnect()
+        print("DONE - disconnecting")
+        bebop.smart_sleep(5)
+        bebop.disconnect()
+
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+##from pyparrot.Bebop import Bebop
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+port = 8080
+s.bind(('', port))  # socketに名前をつける
+print("listening port:"+str(port))
+s.listen(0)  # 接続待ち
+while True:
+    if looping:
+        time.sleep(0.1)
+    else:
+        c, addr = s.accept()  # 接続要求の取り出し
+        print("connected by", addr)
+        Thread(target=loop, args=(c,)).start()
+        time.sleep(0.1)
+
+##bebop = Bebop(drone_type="Bebop2")
